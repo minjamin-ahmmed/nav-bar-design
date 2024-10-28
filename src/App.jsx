6312acde0d00b2e6274ea11e9beba523b50babdf
minjamin-ahmmed <20101058@uap-bd.edu>
2024-10-28 12:26:11 +0600

    environment setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-center py-10 font-bold text-3xl">Today we'll Practice Custom Navbar</h1>
+    </>
+  )
+}
+
+export default App
